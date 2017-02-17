b908c5b444a2f310840c9b18d47b41b64615bd1f
Ankit Deshmukh <ankit.deshmukh@gmail.com>
2017-02-17 18:53:25 -0500

    Fixed merge conflict

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> ff163d659b36a849fe55f3e1d384aae7546abbb8
 var express = require('express'),
     router = express.Router();
 
@@ -9,8 +5,4 @@ router.get('/', function(req, res, next) {
   
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> ff163d659b36a849fe55f3e1d384aae7546abbb8
--- routes/map.js
+++ routes/map.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function(req, res, next) {
-  
+	console.log(req.query.user)
 });
 
 module.exports = router;
