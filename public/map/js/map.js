e28c476d07bf81607187fa1f74865b92fc306739
Jose Canahui <jose.canahui@gmail.com>
2017-04-14 18:59:59 -0400

    added working version of map

--- public/map/js/map.js
+++ public/map/js/map.js
@@ -1,25 +1,24 @@
-require(["js/vendor/ol.js", "dojo/domReady!"],
-        function(ol) {
-          var layers = [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        }),
-        new ol.layer.Tile({
-          extent: [10917802.184872836, 3606569.963093251,
-                    10926233.372078672, 3614097.35912765],
-          source: new ol.source.TileWMS({
-            url: 'http://geo.hokiepark.com:80/geoserver/Parking/wms',
-            params: {'LAYERS': 'Parking:SpaceScrub', 'TILED': true},
-            serverType: 'geoserver'
-          })
-        })
-      ];
-      var map = new ol.Map({
-        layers: layers,
-        target: 'map',
-        view: new ol.View({
-          center: [10917802.184872836, 3606569.963093251],
-          zoom: 12
-        })
-      });
+require(["https://openlayers.org/en/v3.20.1/build/ol.js", "dojo/domReady!"],
+       function(ol) {
+         var layers = [
+       new ol.layer.Tile({
+         source: new ol.source.OSM()
+       }),
+       new ol.layer.Tile({
+         source: new ol.source.TileWMS({
+           url: 'http://geo.hokiepark.com:80/geoserver/Parking/wms',
+           params: {'LAYERS': 'Parking:SpaceScrub', 'TILED': true},
+           serverType: 'geoserver'
+         })
+       })
+     ];
+     var map = new ol.Map({
+       layers: layers,
+       target: 'map',
+       view: new ol.View({
+         center: ol.proj.transform([-80.411235, 37.2133937], 'EPSG:4326','EPSG:3857'),
+         zoom: 15
+       })
+     });
+     map.getView().setCenter(ol.proj.transform([lat, long], 'EPSG:4326', 'EPSG:3857'));
 });
